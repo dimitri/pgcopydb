b2ef0b4f4481874c3aac335bd47e6821785acff9
Dimitri Fontaine <dim@tapoueh.org>
2022-05-03 13:45:04 +0200

    Add a test for the filtering capabilities.

---
+++ tests/filtering/copydb.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -x
+set -e
+
+# This script expects the following environment variables to be set:
+#
+#  - PGCOPYDB_SOURCE_PGURI
+#  - PGCOPYDB_TARGET_PGURI
+#  - PGCOPYDB_TARGET_TABLE_JOBS
+#  - PGCOPYDB_TARGET_INDEX_JOBS
+
+
+#
+# pgcopydb list tables include a retry loop, so we use that as a proxy to
+# depend on the source/target Postgres images to be ready
+#
+pgcopydb list tables --source ${PGCOPYDB_SOURCE_PGURI}
+pgcopydb list tables --source ${PGCOPYDB_TARGET_PGURI}
+
+psql -d ${PGCOPYDB_SOURCE_PGURI} -1 -f /usr/src/pagila/pagila-schema.sql
+psql -d ${PGCOPYDB_SOURCE_PGURI} -1 -f /usr/src/pagila/pagila-data.sql
+
+# pgcopydb copy db uses the environment variables
+pgcopydb copy-db --filters /usr/src/pgcopydb/include.ini
