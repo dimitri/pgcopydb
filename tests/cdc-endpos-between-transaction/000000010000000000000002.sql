cf667b0c4407a8428eec020c49c8bc29ae685c58
Dimitri Fontaine <dim@tapoueh.org>
2023-03-20 15:48:37 +0100

    Use OVERRIDING SYSTEM VALUE in INSERT statements in follow mode. (#214)
    * Use OVERRIDING SYSTEM VALUE in INSERT statements in follow mode.
    
    This allows bypassing GENERATED ALWAYS AS IDENTITY columns when replaying
    changes from the source database to the target database. This is an INSERT
    statement feature.
    
    See https://www.postgresql.org/docs/current/sql-insert.html for details,
    including:
    
      OVERRIDING SYSTEM VALUE
    
      If this clause is specified, then any values supplied for identity columns
      will override the default sequence-generated values.
    
      For an identity column defined as GENERATED ALWAYS, it is an error to
      insert an explicit value (other than DEFAULT) without specifying either
      OVERRIDING SYSTEM VALUE or OVERRIDING USER VALUE. (For an identity column
      defined as GENERATED BY DEFAULT, OVERRIDING SYSTEM VALUE is the normal
      behavior and specifying it does nothing, but PostgreSQL allows it as an
      extension.)
    
    * Fix test files, add comments.

--- tests/cdc-endpos-between-transaction/000000010000000000000002.sql
+++ tests/cdc-endpos-between-transaction/000000010000000000000002.sql
@@ -1,8 +1,8 @@
 BEGIN; -- {"xid":491,"lsn":"0/244B938","timestamp":"2023-03-14 13:54:44.797711+0000"}
-INSERT INTO "public"."category" ("category_id", "name", "last_update") VALUES (1000, 'Fantasy', '2022-12-08 00:00:01+00');
-INSERT INTO "public"."category" ("category_id", "name", "last_update") VALUES (1001, 'History', '2022-12-09 00:00:01+00');
-INSERT INTO "public"."category" ("category_id", "name", "last_update") VALUES (1002, 'Adventure', '2022-12-10 00:00:01+00');
-INSERT INTO "public"."category" ("category_id", "name", "last_update") VALUES (1003, 'Musical', '2022-12-11 00:00:01+00');
-INSERT INTO "public"."category" ("category_id", "name", "last_update") VALUES (1004, 'Western', '2022-12-12 00:00:01+00');
+INSERT INTO "public"."category" ("category_id", "name", "last_update") overriding system value VALUES (1000, 'Fantasy', '2022-12-08 00:00:01+00');
+INSERT INTO "public"."category" ("category_id", "name", "last_update") overriding system value VALUES (1001, 'History', '2022-12-09 00:00:01+00');
+INSERT INTO "public"."category" ("category_id", "name", "last_update") overriding system value VALUES (1002, 'Adventure', '2022-12-10 00:00:01+00');
+INSERT INTO "public"."category" ("category_id", "name", "last_update") overriding system value VALUES (1003, 'Musical', '2022-12-11 00:00:01+00');
+INSERT INTO "public"."category" ("category_id", "name", "last_update") overriding system value VALUES (1004, 'Western', '2022-12-12 00:00:01+00');
 COMMIT; -- {"xid":491,"lsn":"0/244BC38","timestamp":"2023-03-14 13:54:44.797711+0000"}
 -- KEEPALIVE {"lsn":"0/244BD60","timestamp":"2023-03-14 13:54:44.798094+0000"}
