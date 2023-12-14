1bcb61938c359b2bbc62eb49035791b31a4e7fc3
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2023-12-14 21:23:33 +0300

    Apply feedback and simplify code

--- src/bin/pgcopydb/ld_stream.c
+++ src/bin/pgcopydb/ld_stream.c
@@ -92,14 +92,16 @@ stream_init_specs(StreamSpecs *specs,
 		case STREAM_PLUGIN_WAL2JSON:
 		{
 			KeyVal options = {
-				.count = 6,
+				/* we ignore the last keyword and value if the option is not set */
+				.count = specs->slot.wal2jsonNumericAsString ? 7 : 6,
 				.keywords = {
 					"format-version",
 					"include-xids",
 					"include-schemas",
 					"include-transaction",
 					"include-types",
-					"filter-tables"
+					"filter-tables",
+					"numeric-data-types-as-string"
 				},
 				.values = {
 					"2",
@@ -107,21 +109,12 @@ stream_init_specs(StreamSpecs *specs,
 					"true",
 					"true",
 					"true",
-					"pgcopydb.*"
+					"pgcopydb.*",
+					"true"
 				}
 			};
 
-			if (specs->slot.wal2jsonNumericAsString)
-			{
-				int index = options.count;
-
-				options.keywords[index] = "numeric-data-types-as-string";
-				options.values[index] = "true";
-				options.count += 1;
-			}
-
 			specs->pluginOptions = options;
-
 			break;
 		}
 
