10be10693f45c7b3a3a86e362fa59e3ddde15e7b
Dimitri Fontaine <dim@tapoueh.org>
2024-05-16 12:48:04 +0200

    Fix index/constraint lookup when preparing pg_restore list files. (#782)
    The Postgres catalog OIDs are unique per-catalog, not globally unique, and
    we could see the same OID being assigned to both an index and also a
    constraint.
    
    To support the situation, we need to implement per-catalog lookup. In our
    case this is limited to indexes and constraints.

--- src/bin/pgcopydb/dump_restore.c
+++ src/bin/pgcopydb/dump_restore.c
@@ -31,23 +31,58 @@ static bool copydb_copy_database_properties_hook(void *ctx,
 
 
 /*
- * copydb_objectid_has_been_processed_already returns true when a doneFile
- * could be found on-disk for the given target object OID.
+ * copydb_objectid_has_been_processed_already returns true when the given
+ * target object OID is found in our SQLite summary catalogs. This only applies
+ * to indexes or constraints, as TABLE DATA is not part of either the
+ * --pre-data parts of the schema nor the --post-data parts of the schema.
  */
 bool
-copydb_objectid_has_been_processed_already(CopyDataSpec *specs, uint32_t oid)
+copydb_objectid_has_been_processed_already(CopyDataSpec *specs,
+										   ArchiveContentItem *item)
 {
 	DatabaseCatalog *sourceDB = &(specs->catalogs.source);
 
-	bool done = false;
+	uint32_t oid = item->objectOid;
 
-	if (!summary_lookup_oid(sourceDB, oid, &done))
+	switch (item->desc)
 	{
-		/* errors have aleady been logged */
-		return false;
+		case ARCHIVE_TAG_INDEX:
+		{
+			SourceIndex index = { .indexOid = oid };
+			CopyIndexSpec indexSpecs = { .sourceIndex = &index };
+
+			if (!summary_lookup_index(sourceDB, &indexSpecs))
+			{
+				/* errors have aleady been logged */
+				return false;
+			}
+
+			return indexSpecs.summary.doneTime > 0;
+		}
+
+		case ARCHIVE_TAG_CONSTRAINT:
+		{
+			SourceIndex index = { .constraintOid = oid };
+			CopyIndexSpec indexSpecs = { .sourceIndex = &index };
+
+			if (!summary_lookup_constraint(sourceDB, &indexSpecs))
+			{
+				/* errors have aleady been logged */
+				return false;
+			}
+
+			return indexSpecs.summary.doneTime > 0;
+		}
+
+		/* we don't have internal pgcopydb support for other objects */
+		default:
+		{
+			return false;
+		}
 	}
 
-	return done;
+	/* keep compiler happy */
+	return false;
 }
 
 
@@ -685,7 +720,7 @@ copydb_write_restore_list(CopyDataSpec *specs, PostgresDumpSection section)
 			}
 		}
 
-		if (!skip && copydb_objectid_has_been_processed_already(specs, oid))
+		if (!skip && copydb_objectid_has_been_processed_already(specs, item))
 		{
 			skip = true;
 
