5c50c3b1fc0612186acd24271521a0b5363bf067
Ken Barber <kbarber@salesforce.com>
2024-06-13 20:21:33 +1000

    Standalone sequences do not get reset when schema is filtered (#816)
    Because standalone sequences have no table relationships, they were getting skipped.

--- tests/unit/sql/6-sequences.sql
+++ tests/unit/sql/6-sequences.sql
@@ -1,2 +1,5 @@
+select last_value from default_table_id_seq;
 select last_value from identity_table_id_seq;
-select last_value from normal_table_id_seq;
+select last_value from standalone_id_seq;
+select last_value from standalone_smallint_id_seq;
+select last_value from standalone_minvalue_id_seq;
