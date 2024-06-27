3493296dca5444837103842f0ab35cdf8c1d15ea
Cem Eliguzel <cemeliguzel@microsoft.com>
2024-06-27 11:58:01 +0300

    Fixes #813: Error when copying indexes

--- src/bin/pgcopydb/copydb.c
+++ src/bin/pgcopydb/copydb.c
@@ -575,17 +575,16 @@ copydb_init_specs(CopyDataSpec *specs,
 	strlcpy(filter->dbfile, specs->cfPaths.fdbfile, sizeof(filter->dbfile));
 	strlcpy(target->dbfile, specs->cfPaths.tdbfile, sizeof(target->dbfile));
 
-	if (specs->section == DATA_SECTION_ALL ||
-		specs->section == DATA_SECTION_TABLE_DATA)
+	bool shouldCreateVacuumQueue = (specs->section == DATA_SECTION_ALL ||
+									specs->section == DATA_SECTION_INDEXES ||
+									specs->section == DATA_SECTION_TABLE_DATA) &&
+								   !specs->skipVacuum;
+	if (shouldCreateVacuumQueue)
 	{
-		/* create the VACUUM process queue */
-		if (!specs->skipVacuum)
+		if (!queue_create(&(specs->vacuumQueue), "vacuum"))
 		{
-			if (!queue_create(&(specs->vacuumQueue), "vacuum"))
-			{
-				log_error("Failed to create the VACUUM process queue");
-				return false;
-			}
+			log_error("Failed to create the VACUUM process queue");
+			return false;
 		}
 	}
 
