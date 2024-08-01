b87cae7d3a201ab97d531d8ccbc4851e87878b6c
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2024-08-01 21:33:50 +0300

    Remove tlihistfile references

--- src/bin/pgcopydb/copydb.c
+++ src/bin/pgcopydb/copydb.c
@@ -368,10 +368,6 @@ copydb_prepare_filepaths(CopyFilePaths *cfPaths,
 			"%s/slot",
 			cfPaths->cdc.dir);
 
-	sformat(cfPaths->cdc.tlihistfile, MAXPGPATH,
-			"%s/tli.history",
-			cfPaths->cdc.dir);
-
 	sformat(cfPaths->cdc.tlifile, MAXPGPATH,
 			"%s/tli",
 			cfPaths->cdc.dir);
