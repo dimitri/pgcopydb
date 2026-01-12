aa3c73c4ee24af4c5738ed6962eba5c0df3fe2cc
Jeff Mealo <jmealo@protonmail.com>
2026-01-12 14:41:13 -0500

    fix: use cut instead of awk to strip OID column in collations test

--- tests/unit/script/3-collations.sh
+++ tests/unit/script/3-collations.sh
@@ -8,5 +8,4 @@ set -e
 #  - PGCOPYDB_SOURCE_PGURI
 
 # Strip OID column (first column) from output since OIDs vary between databases
-pgcopydb list collations -q --dir /tmp/collations 2>&1 | \
-    awk -F'|' 'NR<=2 {print $2"|"$3} NR>2 {print $2"|"$3}'
+pgcopydb list collations -q --dir /tmp/collations 2>&1 | cut -d'|' -f2-
