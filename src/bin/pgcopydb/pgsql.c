22a0895048bd20dc559a92e774ebcc0417ca5810
Igor Fedorov <igor@fedorov77.ru>
2026-06-08 11:48:04 +0300

    Set GUC values in pgsql_create_logical_replication_slot()
    In the pgsql_create_logical_replication_slot() function, GUC values are not set for the connection, holding a snapshot.
    
    Therefore, if idle_in_transaction_session_timeout is set on the source, the session may be terminated due timeout and the snapshot may be lost.

--- src/bin/pgcopydb/pgsql.c
+++ src/bin/pgcopydb/pgsql.c
@@ -31,6 +31,7 @@
 #include "pg_utils.h"
 #include "signals.h"
 #include "string_utils.h"
+#include "copydb.h"
 
 #if defined(LIBPQ_HAS_PIPELINING) && LIBPQ_HAS_PIPELINING
 #define pqPipelineModeEnabled(conn) (PQpipelineStatus(conn) == PQ_PIPELINE_ON)
@@ -3921,6 +3922,23 @@ pgsql_create_logical_replication_slot(LogicalStreamClient *client,
 	if (!pgsql_open_connection(pgsql))
 	{
 		/* errors have already been logged */
+		return false;
+	}
+
+	/* also set our GUC values for the source connection */
+	if (!pgsql_server_version(pgsql))
+	{
+		/* errors have already been logged */
+		return false;
+	}
+
+	GUC *settings =
+		pgsql->pgversion_num < 90600 ? srcSettings95 : srcSettings;
+
+	if (!pgsql_set_gucs(pgsql, settings))
+	{
+		log_fatal("Failed to set our GUC settings on the source connection, "
+				  "see above for details");
 		return false;
 	}
 
